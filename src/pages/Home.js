0fbab35b7bb3f4c55fce090211a6f58b5cabd6da
Jane Saldana <janesaldana@Janes-Mac-mini.local>
2023-09-28 12:00:32 -0500

    updated product context and filtered products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
+//import prduct context
+import { ProductContext } from '../contexts/ProductContext'
 
 const Home = () => {
-  return <div>Homepage</div>;
+  //get products from product context
+  const { products } = useContext(ProductContext)
+ //get only men's & women's clothing category
+  const filteredProducts = products.filter(item => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );  
+  });
+  
+  return <div>
+
+  </div>;
 };
 
 export default Home;
