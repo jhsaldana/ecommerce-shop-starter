d376615ed5f68dcb0ef0cc91254801f277d046f7
Jane Saldana <janesaldana@Janes-Mac-mini.local>
2023-09-28 22:45:56 -0500

    sidebar, header, and cart updates

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 //product provider
 import ProductProvider from './contexts/ProductContext';
+//sidebar provider
+import SidebarProvider from './contexts/SidebarContext';
+//cartprovider
+import CartProvider from './contexts/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ProductProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ProductProvider>
+  <SidebarProvider>
+    <CartProvider>
+      <ProductProvider>
+       <React.StrictMode>
+          <App />
+       </React.StrictMode>
+      </ProductProvider>  
+    </CartProvider>  
+  </SidebarProvider>
 );
