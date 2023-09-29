7f6be93e7dcaa7fb556796e9ea19ffe9dae3d03f
Jane Saldana <janesaldana@Janes-Mac-mini.local>
2023-09-29 00:52:40 -0500

    cart items and cart context update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,12 +13,12 @@ import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  console.log(useContext(CartContext));
-  return (
+  const { cart } = useContext(CartContext);
+ return (
     <div 
     className={`${
       isOpen ? 'right-0' : '-right-full'
-     }w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg-px-[35px]`}
+     } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg-px-[35px]`}
      >
       <div className='flex items-center justify-between py-6 border-b'>
         <div className='uppercase text-sm font-semibold'>Shopping Bag (0)</div>
@@ -29,6 +29,11 @@ const Sidebar = () => {
          >
           <IoIosArrowForward className='text-2xl' />
         </div>
+      </div>
+      <div>
+        {cart.map((item) => {
+        return <CartItem item={item} key={item.id} />;
+      })}
       </div>    
     </div>
     );
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -9,11 +9,28 @@ const CartProvider = ({ children }) => {
 
   //add to cart
   const addToCart = (product, id)=> {
-    console.log(product);
-    console.log(`${product.title} added to the cart`);
+    const newItem = { ...product, amount: 1 };
+    //check if the item is already in cart
+    const CartItem = cart.find(item => {
+      return item.id === id;
+    })
+    //if item already in cart
+    if (CartItem) {
+      const newCart = [...cart].map(item=> {
+        if (item.id === id) {
+          return {...item, amount: CartItem.amount + 1};
+        } else{
+          return item;
+        }       
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
   };
+
   return (
-  <CartContext.Provider value={{ addToCart }}>
+  <CartContext.Provider value={{ cart, addToCart }}>
     {children}
     </CartContext.Provider>
     );
