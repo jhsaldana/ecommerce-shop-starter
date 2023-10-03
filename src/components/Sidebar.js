a0a70b76734c1a2cc76580ad4cb79f1384eb4746
Jane Saldana <janesaldana@Janes-Mac-mini.local>
2023-10-02 22:37:49 -0500

    sidebar view cart and checkout button and link and footer with copyright note

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -48,6 +48,8 @@ const Sidebar = () => {
             <FiTrash2 />
             </div>
         </div>
+        <Link to="/" className=' bg-gray-200 flex p-4 justify-center items-center text primary w-full font-medium'>View cart</Link>
+        <Link to="/" className=' bg-primary flex p-4 justify-center items-center text-white w-full font-medium'>Checkout</Link>
       </div>
     </div>
     );
